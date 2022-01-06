488123b32d8eb0bc59863cee6948d4c1ae8243ef
BMarcin <m1.borzymowski@gmail.com>
2022-01-06 02:17:13 +0100

    Log the data to the separate files using separated tasks.

--- MiniNeutrinaV2.ino
+++ MiniNeutrinaV2.ino
@@ -12,13 +12,19 @@ int pinBuzzer = 13;
 Adafruit_MPU6050 mpu;
 Adafruit_BMP3XX bmp;
 
-QueueHandle_t queue;
-SdFat sd; 
-File logfile;
-char filename[15];
+QueueHandle_t queueBMP;
+QueueHandle_t queueMPU;
+SdFat sd;
+SdFat sd2; 
+File MPUlogfile;
+File BMPlogfile;
+
+char MPUfilename[14];
+char BMPfilename[14];
+
 float baseAltitude = 0;
 
-struct SensorData
+struct MPUData
 {
     unsigned long currenttime = 0;
     float MPUaccX = -1;
@@ -28,6 +34,11 @@ struct SensorData
     float MPUgyroX = -1;
     float MPUgyroY = -1;
     float MPUgyroZ = -1;
+};
+
+struct BMPData
+{
+    unsigned long currenttime = 0;
 
     float BMPtemperature = -1;
     float BMPpressure = -1;
@@ -40,7 +51,7 @@ void collectMPUData(void * parameters) {
         sensors_event_t a, g, temp;
         mpu.getEvent(&a, &g, &temp);
 
-        SensorData sensData;
+        MPUData sensData;
 
         sensData.currenttime = millis();
         sensData.MPUaccX = a.acceleration.x;
@@ -50,7 +61,7 @@ void collectMPUData(void * parameters) {
         sensData.MPUgyroY = g.gyro.y;
         sensData.MPUgyroZ = g.gyro.z;
 
-        xQueueSend(queue, &sensData, portMAX_DELAY);
+        xQueueSend(queueMPU, &sensData, portMAX_DELAY);
         vTaskDelay(1 / portTICK_PERIOD_MS);
     }
 }
@@ -59,54 +70,80 @@ void collectBMPData(void * parameters) {
     for (;;) {
         bmp.performReading();
 
-        SensorData sensData;
+        BMPData sensData;
 
         sensData.currenttime = millis();
         sensData.BMPtemperature = bmp.temperature;
         sensData.BMPpressure = bmp.pressure / 100.0; //hPa
         sensData.BMPaltitude = bmp.readAltitude(SEALEVELPRESSURE_HPA) - baseAltitude;
 
-        xQueueSend(queue, &sensData, portMAX_DELAY);
+        xQueueSend(queueBMP, &sensData, portMAX_DELAY);
         vTaskDelay(1 / portTICK_PERIOD_MS);
     }
 }
 
-void logData(void * parameters) {
-    logfile = sd.open(filename, O_CREAT | O_APPEND | O_WRITE);
+void logMPUData(void * parameters) {
+    MPUlogfile = sd.open(MPUfilename, O_CREAT | O_APPEND | O_WRITE);
     for (;;) {
         // Serial.print("Task 2 counter: ");
         // Serial.println(count2++);
-        SensorData element;
-        xQueueReceive(queue, &element, portMAX_DELAY);
+        MPUData element;
+        xQueueReceive(queueMPU, &element, portMAX_DELAY);
 
         
-        logfile.print(element.currenttime);
-        logfile.print(",");
-        logfile.print(element.MPUaccX);
-        logfile.print(",");
-        logfile.print(element.MPUaccY);
-        logfile.print(",");
-        logfile.print(element.MPUaccZ);
-        logfile.print(",");
-        logfile.print(element.MPUgyroX);
-        logfile.print(",");
-        logfile.print(element.MPUgyroY);
-        logfile.print(",");
-        logfile.print(element.MPUgyroZ);
-        logfile.print(",");
-        logfile.print(element.BMPaltitude);
-        logfile.print(",");
-        logfile.print(element.BMPpressure);
-        logfile.print(",");
-        logfile.println(element.BMPtemperature);
+        MPUlogfile.print(element.currenttime);
+        MPUlogfile.print(",");
+        MPUlogfile.print(element.MPUaccX);
+        MPUlogfile.print(",");
+        MPUlogfile.print(element.MPUaccY);
+        MPUlogfile.print(",");
+        MPUlogfile.print(element.MPUaccZ);
+        MPUlogfile.print(",");
+        MPUlogfile.print(element.MPUgyroX);
+        MPUlogfile.print(",");
+        MPUlogfile.print(element.MPUgyroY);
+        MPUlogfile.print(",");
+        MPUlogfile.println(element.MPUgyroZ);
+        // logfile.print(",");
+        // logfile.print(element.BMPaltitude);
+        // logfile.print(",");
+        // logfile.print(element.BMPpressure);
+        // logfile.print(",");
+        // logfile.println(element.BMPtemperature);
         // logfile.write((const uint8_t *)&element, sizeof(element));
 
         digitalWrite(pinBuzzer, LOW);
-        logfile.flush();
+        MPUlogfile.flush();
         digitalWrite(pinBuzzer, HIGH);
         // vTaskDelay(1 / portTICK_PERIOD_MS);
     }
-    logfile.close();
+    MPUlogfile.close();
+}
+
+void logBMPData(void * parameters) {
+    BMPlogfile = sd2.open(BMPfilename, O_CREAT | O_APPEND | O_WRITE);
+    for (;;) {
+        // Serial.print("Task 2 counter: ");
+        // Serial.println(count2++);
+        BMPData element;
+        xQueueReceive(queueBMP, &element, portMAX_DELAY);
+
+        
+        BMPlogfile.print(element.currenttime);
+        BMPlogfile.print(",");
+        BMPlogfile.print(element.BMPaltitude);
+        BMPlogfile.print(",");
+        BMPlogfile.print(element.BMPpressure);
+        BMPlogfile.print(",");
+        BMPlogfile.println(element.BMPtemperature);
+        // logfile.write((const uint8_t *)&element, sizeof(element));
+
+        digitalWrite(LED_BUILTIN, LOW);
+        BMPlogfile.flush();
+        digitalWrite(LED_BUILTIN, HIGH);
+        vTaskDelay(10 / portTICK_PERIOD_MS);
+    }
+    BMPlogfile.close();
 }
 
 void beep(int beep_time) {
@@ -171,47 +208,76 @@ void setup()
     // ---------------------------------------------------------------
 
     Serial.print("sd Card state: ");
-    if (!sd.begin(5, SD_SCK_MHZ(11))) {
+    if (!sd.begin(5, SD_SCK_MHZ(15))) {
+        error();
+    }
+    Serial.println("Ok!");
+
+    Serial.print("sd Card state: ");
+    if (!sd2.begin(5, SD_SCK_MHZ(15))) {
         error();
     }
     Serial.println("Ok!");
 
     
-    strcpy(filename, "/DATA00.TXT");
+    strcpy(BMPfilename, "/BMP00.TXT");
+    strcpy(MPUfilename, "/MPU00.TXT");
     for (uint8_t i = 0; i < 100; i++) {
-        filename[5] = '0' + i/10;
-        filename[6] = '0' + i%10;
+        BMPfilename[4] = '0' + i/10;
+        BMPfilename[5] = '0' + i%10;
 
-        if (! sd.exists(filename)) {
+        MPUfilename[4] = '0' + i/10;
+        MPUfilename[5] = '0' + i%10;
+
+        if (! sd.exists(BMPfilename)) {
             break;
         }
     }
 
     // ---------------------------------------------------------------
 
-    queue = xQueueCreate(2000, sizeof( SensorData ) );
-    if(queue == NULL){
-        Serial.println("Error creating the queue");
+    queueMPU = xQueueCreate(2000, sizeof( MPUData ) );
+    if(queueMPU == NULL){
+        Serial.println("Error creating the queueMPU");
         error();
     }
 
     // ---------------------------------------------------------------
 
-    logfile = sd.open(filename, O_WRITE | O_CREAT |O_TRUNC); 
-    if( ! logfile ) {
-        Serial.print("Couldnt create "); 
-        Serial.println(filename);
+    queueBMP = xQueueCreate(1000, sizeof( BMPData ) );
+    if(queueBMP == NULL){
+        Serial.println("Error creating the queueBMP");
         error();
     }
-    Serial.print("Ready to write to:"); Serial.println(filename);
-    logfile.close();
+
+    // ---------------------------------------------------------------
+
+    MPUlogfile = sd.open(MPUfilename, O_WRITE | O_CREAT |O_TRUNC); 
+    if( ! MPUlogfile ) {
+        Serial.print("Couldnt create "); 
+        Serial.println(MPUfilename);
+        error();
+    }
+    Serial.print("Ready to write to:"); Serial.println(MPUfilename);
+    MPUlogfile.close();
+
+    // ---------------------------------------------------------------
+
+    BMPlogfile = sd.open(BMPfilename, O_WRITE | O_CREAT |O_TRUNC); 
+    if( ! BMPlogfile ) {
+        Serial.print("Couldnt create "); 
+        Serial.println(BMPfilename);
+        error();
+    }
+    Serial.print("Ready to write to:"); Serial.println(BMPfilename);
+    BMPlogfile.close();
 
     // ---------------------------------------------------------------
 
 	xTaskCreate(
         collectMPUData,
         "collectMPUData",
-        14000,
+        16000,
         NULL,
         1,
         NULL
@@ -220,16 +286,25 @@ void setup()
     xTaskCreate(
         collectBMPData,
         "collectBMPData",
-        14000,
+        16000,
         NULL,
         1,
         NULL
     );
 
     xTaskCreate(
-        logData,
-        "LogData",
-        16000,
+        logMPUData,
+        "logMPUData",
+        18000,
+        NULL,
+        1,
+        NULL
+    );
+
+    xTaskCreate(
+        logBMPData,
+        "logBMPData",
+        18000,
         NULL,
         1,
         NULL
